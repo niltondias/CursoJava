379f10a14ce687fd3582190f94caf560ea7e2a2c
nilton <ntjdias@gmail.com>
2016-08-18 23:42:52 -0300

    teste Array

--- CursoJava/src/curso/collections/Aluno.java
+++ CursoJava/src/curso/collections/Aluno.java
@@ -1,6 +1,27 @@
 package curso.collections;
 
 public class Aluno {
-	String nome;
+	private String nome;
+	private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}   
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public Aluno( String nome) {
+		this.nome = nome;
+	}
 
 }
--- CursoJava/src/curso/collections/TesteArray.java
+++ CursoJava/src/curso/collections/TesteArray.java
@@ -1,5 +1,30 @@
 package curso.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TesteArray {
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Joaquina");
+		Aluno a2 = new Aluno("Sandra");
+		Aluno a3 = new Aluno("Marcos");
+		Aluno a4 = new Aluno("Miguel");
+		Aluno a5 = new Aluno("Jonas");
+		
+		List<Aluno> alunos = new ArrayList<Aluno>();
+		alunos.add(a1);
+		alunos.add(a2);
+		alunos.add(a3);
+		alunos.add(a4);
+		alunos.add(a5);
+		
+		ImprimeAlunos( alunos );
+	}
+	
+	public static void ImprimeAlunos( List<Aluno> alunos) {
+		for( Aluno a : alunos) {
+			System.out.println("Nome do aluno: " + a.getNome());
+		}
+	}
 
 }
