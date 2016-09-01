e837687d392cd8e3312120050784a07471bd6bfd
nilton <ntjdias@gmail.com>
2016-08-31 23:29:03 -0300

    Log4J exemplos

---
+++ CursoJava/src/curso/logging/ExemploSalvandoCliente.java
@@ -0,0 +1,26 @@
+package curso.logging;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import curso.logging.dao.ClienteDAO;
+import curso.logging.model.Cliente;
+
+public class ExemploSalvandoCliente {
+
+	private static Logger logger = Logger.getLogger(ExemploSalvandoCliente.class);
+	
+	public static void main(String[] args) {
+		PropertyConfigurator.configure("log4j.properties");
+		
+		logger.info("Iniciando a aplicação");
+		
+		Cliente cliente = new Cliente("João");
+		new ClienteDAO().salvar(cliente);
+		
+		new ClienteDAO().salvar(null);
+		
+		logger.info("Finalizando a aplicação.");
+	}
+	
+}
